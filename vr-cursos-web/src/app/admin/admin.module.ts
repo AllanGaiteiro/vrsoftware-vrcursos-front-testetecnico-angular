464ba616cd140de3c70b6a3a157399c582b377e2
Allan Gaiteiro <64567248+AllanGaiteiro@users.noreply.github.com>
2022-08-01 03:03:53 -0300

    create route for courses

--- vr-cursos-web/src/app/admin/admin.module.ts
+++ vr-cursos-web/src/app/admin/admin.module.ts
@@ -4,19 +4,23 @@ import { RouterModule } from '@angular/router';
 
 import { AdminRoutes } from './admin.routes';
 
-import { CoursesComponent } from './pages/courses/courses.component';
 import { StudentsComponent } from './pages/students/students.component';
 import { RegistrationComponent } from './pages/registration/registration.component';
 import { SharedModule } from '../shared/shared.module';
+import { CourseListComponent } from './pages/courses/course-list/course-list.component';
+import { CourseViewComponent } from './pages/courses/course-view/course-view.component';
+import { CourseCreateComponent } from './pages/courses/course-create/course-create.component';
 
 
 
 
 @NgModule({
   declarations: [
-    CoursesComponent,
     StudentsComponent,
-    RegistrationComponent
+    RegistrationComponent,
+    CourseListComponent,
+    CourseViewComponent,
+    CourseCreateComponent
   ],
   imports: [
     CommonModule,
--- vr-cursos-web/src/app/admin/admin.routes.ts
+++ vr-cursos-web/src/app/admin/admin.routes.ts
@@ -1,19 +1,44 @@
 import { Routes } from '@angular/router';
-import { CoursesComponent } from './pages/courses/courses.component';
+import { CourseCreateComponent } from './pages/courses/course-create/course-create.component';
+import { CourseListComponent } from './pages/courses/course-list/course-list.component';
+import { CourseViewComponent } from './pages/courses/course-view/course-view.component';
 import { RegistrationComponent } from './pages/registration/registration.component';
 import { StudentsComponent } from './pages/students/students.component';
 
 export const AdminRoutes: Routes = [
   {
     path: 'cursos',
-    component: CoursesComponent
+    children: [
+      {
+        path: 'listar',
+        component: CourseListComponent
+      },
+      {
+        path: 'ver',
+        component: CourseViewComponent
+      },
+      {
+        path: 'criar',
+        component: CourseCreateComponent
+      },
+    ]
   },
   {
     path: 'alunos',
-    component: StudentsComponent
+    children: [
+      {
+        path: 'listar',
+        component: StudentsComponent
+      }
+    ]
   },
   {
     path: 'registros',
-    component: RegistrationComponent
-  },
+    children: [
+      {
+        path: 'listar',
+        component: RegistrationComponent
+      }
+    ]
+  }
 ];
