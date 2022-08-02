869ca041bfc9a71f6682eaed39a88ab4c820b0a0
Allan Gaiteiro <64567248+AllanGaiteiro@users.noreply.github.com>
2022-08-02 08:38:01 -0300

    add form basic in component couses create

--- vr-cursos-web/src/app/admin/pages/courses/course-create/course-create.component.ts
+++ vr-cursos-web/src/app/admin/pages/courses/course-create/course-create.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { Course } from 'src/app/core/models/course/course';
 
 @Component({
   selector: 'app-course-create',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./course-create.component.scss']
 })
 export class CourseCreateComponent implements OnInit {
-
-  constructor() { }
-
+  formFields: { name: string, value: string }[];
+  formGroup: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.formGroup = this.createFormCourse(new Course());
+    this.formFields = [
+      { name: 'descricao', value: 'Descrição' },
+      { name: 'ementa', value: 'Ementa' }
+    ];
+  }
   ngOnInit(): void {
   }
 
+  createFormCourse(course: Course) {
+
+    return this.fb.group({
+      descricao: new FormControl(course?.descricao ?? '', [Validators.required]),
+      ementa: new FormControl(course?.ementa ?? '', [Validators.required])
+    })
+  }
+
+  createCourse(){
+    
+  }
 }
