b6fbee03386811a946dab7f4c75f8a02cb0868b2
Allan Gaiteiro <64567248+AllanGaiteiro@users.noreply.github.com>
2022-08-02 19:11:34 -0300

    add form in component create course

--- vr-cursos-web/src/app/admin/pages/courses/course-create/course-create.component.ts
+++ vr-cursos-web/src/app/admin/pages/courses/course-create/course-create.component.ts
@@ -8,27 +8,28 @@ import { Course } from 'src/app/core/models/course/course';
   styleUrls: ['./course-create.component.scss']
 })
 export class CourseCreateComponent implements OnInit {
-  formFields: { name: string, value: string }[];
+  formFields: {
+    name: string, value: string, type: string
+  }[];
   formGroup: FormGroup;
   constructor(private fb: FormBuilder) {
     this.formGroup = this.createFormCourse(new Course());
     this.formFields = [
-      { name: 'descricao', value: 'Descrição' },
-      { name: 'ementa', value: 'Ementa' }
+      { name: 'descricao', value: 'Descrição', type: 'input' },
+      { name: 'ementa', value: 'Ementa', type: 'textarea' }
     ];
   }
   ngOnInit(): void {
   }
 
-  createFormCourse(course: Course) {
-
+  createFormCourse(course: Course): FormGroup {
     return this.fb.group({
       descricao: new FormControl(course?.descricao ?? '', [Validators.required]),
       ementa: new FormControl(course?.ementa ?? '', [Validators.required])
     })
   }
 
-  createCourse(){
-    
+  createCourse(course: Course): void {
+    console.log(course)
   }
 }
