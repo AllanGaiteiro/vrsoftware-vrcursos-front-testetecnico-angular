b75bda8695b4260d6ae89b215ee091cf8c51dbee
Allan Gaiteiro <64567248+AllanGaiteiro@users.noreply.github.com>
2022-08-02 19:10:26 -0300

    fix form basic and add buttons

--- vr-cursos-web/src/app/shared/form-basic/form-basic.component.ts
+++ vr-cursos-web/src/app/shared/form-basic/form-basic.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 
 @Component({
@@ -7,10 +7,17 @@ import { FormGroup } from '@angular/forms';
   styleUrls: ['./form-basic.component.scss']
 })
 export class FormCoursesComponent implements OnInit {
-  @Input() formFields:{name:string,value:string}[] = [];
+  @Input() formFields: {
+    name: string, value: string, type: string
+  }[] = [];
   @Input() formGroup: FormGroup = new FormGroup({});
   @Input() formName: string = '';
+  @Input() actions: string[] = [];
+  @Output() newEventSubmit = new EventEmitter<any>();
 
+  onSubmit() {
+    this.newEventSubmit.emit(this.formGroup.getRawValue());
+  }
   ngOnInit(): void {
   }
 
