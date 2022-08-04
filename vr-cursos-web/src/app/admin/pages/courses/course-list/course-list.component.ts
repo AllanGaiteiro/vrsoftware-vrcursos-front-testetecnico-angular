98774e37330d6289e198e3650a1a1b9286235c72
Allan Gaiteiro <64567248+AllanGaiteiro@users.noreply.github.com>
2022-08-03 21:14:06 -0300

    remove coment

--- vr-cursos-web/src/app/admin/pages/courses/course-list/course-list.component.ts
+++ vr-cursos-web/src/app/admin/pages/courses/course-list/course-list.component.ts
@@ -1,22 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-export interface PeriodicElement {
-  name: string;
-  codigo: number;
-
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  { codigo: 1, name: 'Hydrogen' },
-  { codigo: 2, name: 'Helium' },
-  { codigo: 3, name: 'Lithium' },
-  { codigo: 4, name: 'Beryllium' },
-  { codigo: 5, name: 'Boron' },
-  { codigo: 6, name: 'Carbon' },
-  { codigo: 7, name: 'Nitrogen' },
-  { codigo: 8, name: 'Oxygen' },
-  { codigo: 9, name: 'Fluorine' },
-  { codigo: 10, name: 'Neon' },
-];
+import { Subscription } from 'rxjs';
+import { Course } from 'src/app/core/models/course/course';
+import { CourseService } from '../course.service';
 
 @Component({
   selector: 'app-course-list',
@@ -27,16 +12,27 @@ export class CourseListComponent implements OnInit {
   displayedColumnsObj: { name: string, value: string }[] =
     [
       { name: 'codigo', value: "Codigo" },
-      { name: 'name', value: "Nome" }
+      { name: 'descricao', value: "Descrição" },
     ];
-  displayedColumns: string[] = []
-  dataSource = ELEMENT_DATA;
-  constructor() {
-    console.log('app-courses', this.displayedColumnsObj);
-    console.log('app-courses', this.dataSource);
+  courseSubscription?: Subscription;
+  displayedColumns: string[] = [];
+  dataSource: Course[] = [];
+  constructor(private service: CourseService) {
   }
 
   ngOnInit(): void {
+    // Course Find
+    this.courseSubscription = this.service.find().subscribe((res) => {
+      this.dataSource = res;
+    }, (error) => {
+      console.error('Course Find One - Error ocurred', error)
+    })
+  }
+
+  ngOnDestroy(): void {
+    if (this.courseSubscription) {
+      this.courseSubscription.unsubscribe()
+    }
   }
 
 }
