71156a3776878ad979201476c3a5b0ac4e6214ed
Allan Gaiteiro <64567248+AllanGaiteiro@users.noreply.github.com>
2022-08-05 08:30:01 -0300

    fix style table basic

--- vr-cursos-web/src/app/shared/table-basic/table-basic.component.ts
+++ vr-cursos-web/src/app/shared/table-basic/table-basic.component.ts
@@ -7,11 +7,10 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./table-basic.component.scss'],
 })
 export class TableBasicComponent implements OnInit {
-  @Input() displayedColumnsObj: { name: string, value: string }[] = [];
+  @Input() displayedColumnsObj: { name: string, value: string, length: number }[] = [];
   @Input() data: any[] = [];
   @Input() actions: string[] = [];
   displayedColumns: string[] = []
-  classColumn: string = 'col-4';
   constructor() {
   }
 
@@ -19,17 +18,16 @@ export class TableBasicComponent implements OnInit {
 
     // add column action
     if (this.actions.length > 0) {
-      this.displayedColumnsObj.push({ name: 'actions', value: 'Ações' });
+      this.displayedColumnsObj.push({ name: 'actions', value: 'Ações', length: 2 });
     }
     this.displayedColumns = this.getDisplayedColumns();
-    this.classColumn = this.getClassColumn();
   }
 
   getDisplayedColumns(): string[] {
     return this.displayedColumnsObj.length > 0 ? this.displayedColumnsObj.map(({ name }) => name) : [];
   }
 
-  getClassColumn(): string {
-    return 'col-' + (12 / this.displayedColumnsObj.length)
+  btnAction(action: string) {
+    console.log(action)
   }
 }
