284ab52b8b1ba32d0231ffbff0552b8f0bde18af
Allan Gaiteiro <64567248+AllanGaiteiro@users.noreply.github.com>
2022-08-06 11:47:01 -0300

    create service matriculation

---
+++ vr-cursos-web/src/app/admin/pages/matriculations/matriculation.service.ts
@@ -0,0 +1,47 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CreateMatriculationDto } from 'src/app/core/models/matriculation/dto/create-matriculation.dto';
+import { Matriculation } from 'src/app/core/models/matriculation/entities/matriculation.entity';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MatriculationService {
+  resource: string = 'matriculations';
+  constructor(private http: HttpClient) { }
+
+  find(filter?: any): Observable<Matriculation[]> {
+    const params = filter ? new HttpParams().append('filter', filter) : '';
+    return this.http.get<Matriculation[]>(`${environment.backEndUrl}/${this.resource}` + params);
+  }
+
+  findOne(id: number): Observable<Matriculation> {
+    return this.http.get<Matriculation>(`${environment.backEndUrl}/${this.resource}/${id}`);
+  }
+
+  async create(data: CreateMatriculationDto): Promise<Matriculation> {
+    return new Promise((resolve, rejects) => {
+      this.http.post<Matriculation>(`${environment.backEndUrl}/${this.resource}/`, data).subscribe(res => {
+        resolve(res)
+      })
+    })
+  }
+
+  update(id: number, data: Partial<CreateMatriculationDto>): Promise<Matriculation> {
+    return new Promise((resolve, rejects) => {
+      this.http.patch<Matriculation>(`${environment.backEndUrl}/${this.resource}/${id}`, data).subscribe(res => {
+        resolve(res)
+      })
+    })
+  }
+
+  delete(id: number): Promise<void> {
+    return new Promise((resolve, rejects) => {
+      this.http.delete<void>(`${environment.backEndUrl}/${this.resource}/${id}`).subscribe(res => {
+        resolve()
+      })
+    })
+  }
+}
