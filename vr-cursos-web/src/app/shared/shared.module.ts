5039482f80e79b5f2ff4dbf1d13886d57eb4c7e8
Allan Gaiteiro <64567248+AllanGaiteiro@users.noreply.github.com>
2022-08-07 15:20:45 -0300

    remove material modules of shared module

--- vr-cursos-web/src/app/shared/shared.module.ts
+++ vr-cursos-web/src/app/shared/shared.module.ts
@@ -1,24 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import {MatDialogModule} from '@angular/material/dialog';
-import { MatTableModule } from '@angular/material/table';
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
-import {MatButtonToggleModule} from '@angular/material/button-toggle';
-import {MatIconModule} from '@angular/material/icon';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select';
 import { ReactiveFormsModule } from '@angular/forms';
-import {MatExpansionModule} from '@angular/material/expansion';
-import {MatListModule} from '@angular/material/list';
+import { HttpClientModule } from '@angular/common/http';
+import { MaterialModule } from './material.module';
 
 import { ButtonComponent } from './button/button.component';
 import { TableBasicComponent } from './table-basic/table-basic.component';
 import { FormCoursesComponent } from './form-basic/form-basic.component';
-import { HttpClientModule } from '@angular/common/http';
+
 import { Menu } from '../core/menu-admin/Menu';
+
 @NgModule({
   declarations: [
     TableBasicComponent,
@@ -28,38 +19,18 @@ import { Menu } from '../core/menu-admin/Menu';
   imports: [
     CommonModule,
     HttpClientModule,
-    MatTableModule,
-    MatCardModule,
-    MatButtonModule,
-    MatButtonToggleModule,
-    MatIconModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatSelectModule,
-    MatExpansionModule,
-    MatDialogModule,
-    MatListModule,
+    MaterialModule,
     ReactiveFormsModule
   ],
   exports: [
     CommonModule,
     HttpClientModule,
-    MatTableModule,
-    MatCardModule,
-    MatButtonModule,
-    MatButtonToggleModule,
-    MatIconModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatSelectModule,
-    MatExpansionModule,
-    MatDialogModule,
-    MatListModule,
+    MaterialModule,
     ReactiveFormsModule,
     TableBasicComponent,
     ButtonComponent,
     FormCoursesComponent,
   ],
-  providers:[Menu]
+  providers: [Menu]
 })
 export class SharedModule { }
