dbda22d3931cccce2f193b84fda922f1e6bf8dab
Allan Gaiteiro <64567248+AllanGaiteiro@users.noreply.github.com>
2022-08-07 23:06:44 -0300

    add modules matdatasource, matsort,  matpaginator and dialog

--- vr-cursos-web/src/app/shared/shared.module.ts
+++ vr-cursos-web/src/app/shared/shared.module.ts
@@ -2,19 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { MaterialModule } from './material.module';
+import { MaterialModule } from './modules/material.module';
 
 import { ButtonComponent } from './button/button.component';
 import { TableBasicComponent } from './table-basic/table-basic.component';
 import { FormCoursesComponent } from './form-basic/form-basic.component';
 
 import { Menu } from '../core/menu-admin/Menu';
+import { DialogComfirmComponent } from './dialog-comfirm/dialog-comfirm.component';
 
 @NgModule({
   declarations: [
     TableBasicComponent,
     ButtonComponent,
     FormCoursesComponent,
+    DialogComfirmComponent,
   ],
   imports: [
     CommonModule,
