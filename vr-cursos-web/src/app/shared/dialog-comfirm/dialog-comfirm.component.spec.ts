aa8beabda102b9890acdbc05cedbb399157ff28e
Allan Gaiteiro <64567248+AllanGaiteiro@users.noreply.github.com>
2022-08-07 23:07:50 -0300

    criate dialog comfirm

---
+++ vr-cursos-web/src/app/shared/dialog-comfirm/dialog-comfirm.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DialogComfirmComponent } from './dialog-comfirm.component';
+
+describe('DialogComfirmComponent', () => {
+  let component: DialogComfirmComponent;
+  let fixture: ComponentFixture<DialogComfirmComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ DialogComfirmComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DialogComfirmComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
