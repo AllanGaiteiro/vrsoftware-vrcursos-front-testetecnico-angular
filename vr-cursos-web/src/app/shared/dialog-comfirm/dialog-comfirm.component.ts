95db366d87d83faf7e4927de0f15126a8ff7c769
Allan Gaiteiro <64567248+AllanGaiteiro@users.noreply.github.com>
2022-08-08 01:39:03 -0300

    create mat dialog

--- vr-cursos-web/src/app/shared/dialog-comfirm/dialog-comfirm.component.ts
+++ vr-cursos-web/src/app/shared/dialog-comfirm/dialog-comfirm.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { MatDialogRef } from '@angular/material/dialog';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-dialog-comfirm',
@@ -7,11 +7,21 @@ import { MatDialogRef } from '@angular/material/dialog';
   styleUrls: ['./dialog-comfirm.component.scss']
 })
 export class DialogComfirmComponent implements OnInit {
+  public title = 'Dialog Comfirm';
+  public description = 'Dialog Comfirm 2';
+  public isOnlyConfirm: boolean = false;
 
-  constructor(public dialogRef: MatDialogRef<DialogComfirmComponent>) { }
+
+  constructor(public dialogRef: MatDialogRef<DialogComfirmComponent>,
+    @Inject(MAT_DIALOG_DATA) data: any) {
+    this.title = data.title;
+    this.description = data.description;
+    this.isOnlyConfirm = data.isOnlyConfirm;
+  }
 
   ngOnInit(): void {
   }
+
 
 }
 
